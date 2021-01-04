d92649b8b38c81294b1366e0ddc0ec30ea202701
EndMy5uffering <havemph@hotmail.de>
2021-01-04 18:45:07 +0100

    Added time state and swap to TimeSwapInput.cs

--- IV-TimeGame/IV-TimeGame/Assets/Scripts/TimeSwapInput.cs
+++ IV-TimeGame/IV-TimeGame/Assets/Scripts/TimeSwapInput.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class TimeSwapInput : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public KeyCode timeToggleKey = KeyCode.C;
+
+    //State 0 = past; State 1 = present
+    private int state = 0;
+
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(timeToggleKey))
+        {
+            //toggles between states
+            state = state == 0 ? 1 : 0;
+        }
     }
+
+    public int getTimeState() 
+    {
+        return this.state;
+    }
+
 }
