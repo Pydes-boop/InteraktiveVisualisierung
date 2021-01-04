d46740d10015b8d158275414e7f8afb16e1ce0fc
EndMy5uffering <havemph@hotmail.de>
2021-01-04 19:32:11 +0100

    changed time swap script to event based

--- IV-TimeGame/IV-TimeGame/Assets/Scripts/TimeSwapInput.cs
+++ IV-TimeGame/IV-TimeGame/Assets/Scripts/TimeSwapInput.cs
@@ -6,24 +6,25 @@ public class TimeSwapInput : MonoBehaviour
 {
     public KeyCode timeToggleKey = KeyCode.C;
 
+    public delegate void TimeToggle(int state);
+    public event TimeToggle OnTimeToggle;
+
     //State 0 = past; State 1 = present
     private int state = 0;
-
-    void Start()
-    {
-
-    }
 
     void Update()
     {
         if (Input.GetKeyDown(timeToggleKey))
         {
-            //toggles between states
+            //change state
             state = state == 0 ? 1 : 0;
+
+            //notify subscribers
+            OnTimeToggle?.Invoke(state);
         }
     }
 
-    public int getTimeState() 
+    public int GetTimeState() 
     {
         return this.state;
     }
