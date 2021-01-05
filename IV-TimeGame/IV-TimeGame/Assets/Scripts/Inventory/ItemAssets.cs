c919d86bcecabccca316e2f902738b8c7d087cd1
ge38fet <marina.weber@tum.de>
2021-01-05 17:16:26 +0100

    Added basic layout for inventory

---
+++ IV-TimeGame/IV-TimeGame/Assets/Scripts/Inventory/ItemAssets.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemAssets : MonoBehaviour
+{
+    public static ItemAssets Instance { get; private set; }
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public Sprite NoteSprite;
+    public Sprite KeySprite;
+    public Sprite NoteIcon;
+    public Sprite KeyIcon;
+    public Sprite OtherIcon;
+}
