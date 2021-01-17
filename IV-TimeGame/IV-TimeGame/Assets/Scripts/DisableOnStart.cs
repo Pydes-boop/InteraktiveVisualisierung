396909bd2ca64e9a1d6f2f83238e2567b6e3e833
Marina Weber <marina.weber@tum.de>
2021-01-17 18:25:21 +0100

    disable rumprobiert

--- IV-TimeGame/IV-TimeGame/Assets/Scripts/DisableOnStart.cs
+++ IV-TimeGame/IV-TimeGame/Assets/Scripts/DisableOnStart.cs
@@ -7,8 +7,15 @@ public class DisableOnStart : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.SetActive(false);
+        this.gameObject.SetActive(false);
+        StartCoroutine(Disable());
+    }
+    IEnumerator Disable()
+    {
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(0.05f);
+        this.gameObject.SetActive(false);
     }
 
-  
 }
