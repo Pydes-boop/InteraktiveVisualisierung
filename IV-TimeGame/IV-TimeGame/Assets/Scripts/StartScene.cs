ffba9f7aa41c1a440e0eb3c3aeff6807e17562fb
Marina Weber <marina.weber@tum.de>
2021-01-18 03:01:19 +0100

    added potential Start Scene

---
+++ IV-TimeGame/IV-TimeGame/Assets/Scripts/StartScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartScene : MonoBehaviour
+{
+    public Button startButton;
+    public string nextScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(SwitchScene);
+    }
+
+    // Update is called once per frame
+     private void SwitchScene()
+    {
+        SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
+        
+    }
+}
